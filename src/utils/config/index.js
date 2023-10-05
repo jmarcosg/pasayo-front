9871f97c924a365abac639bbf337e9efd606c6e1
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-04 21:21:13 -0300

    🩹 fix and hide dotenv

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,2 +1,2 @@
-export const URL_BACK = import.meta.env.VITE_URL_BACK || 'http://localhost:4000/api';
-export const APP_NAME = import.meta.env.VITE_APP_NAME || 'pasayo';
+export const URL_BACK = import.meta.env.VITE_URL_BACK;
+export const APP_NAME = import.meta.env.VITE_APP_NAME;
