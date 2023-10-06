5a6301fdc5c7c021c35f45053b9605500ed53cdc
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-05 23:42:45 -0300

    ✨ add topic filter

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Input } from './Input';
+export { default as TopicFilter } from './Filter/Topic';
 export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
