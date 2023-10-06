2c281d81759c287f55f4449204687656de339d12
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-05 23:43:08 -0300

    🚧 community page

---
+++ src/pages/Community/index.jsx
@@ -0,0 +1,15 @@
+import { TopicFilter } from '../../components';
+
+const Community = () => {
+  return (
+    <>
+      <TopicFilter />
+
+      <div className='container'>
+        <h1>Community</h1>
+      </div>
+    </>
+  );
+};
+
+export default Community;
