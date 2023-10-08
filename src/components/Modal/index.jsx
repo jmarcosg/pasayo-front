c781e49f14c79b2968e507541b740f6477b7f6a9
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-08 19:21:50 -0300

    ✨ add modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,17 @@
+import { Modal as M } from 'react-bootstrap';
+
+const Modal = ({ size, show, setShow, title, footer, children, loading }) => {
+  const hide = () => setShow(false);
+
+  return (
+    <M centered aria-labelledby='contained-modal-title-vcenter' show={show} size={size ? size : 'lg'} onHide={hide}>
+      <M.Header closeButton>
+        <M.Title id='contained-modal-title-vcenter'>{!loading && title()}</M.Title>
+      </M.Header>
+      <M.Body>{children}</M.Body>
+      {footer && <M.Footer>{!loading && footer()}</M.Footer>}
+    </M>
+  );
+};
+
+export default Modal;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { Input } from './Input';
-export { default as TopicFilter } from './Filter/Topic';
 export { default as Layout } from './Layout';
+export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
+export { default as TopicFilter } from './Filter/Topic';
