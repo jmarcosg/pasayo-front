2f1bd82d23f698e08cb82f515d90368457f13307
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-08 18:41:09 -0300

    ✨ improve filter navbar

--- src/components/Filter/Topic/handlers.js
+++ src/components/Filter/Topic/handlers.js
@@ -10,3 +10,16 @@ export const authors = [
   { name: 'TODAS', value: 'TODAS' },
   { name: 'MIAS', value: 'MIAS' },
 ];
+
+export const getData = async (state, setState) => {
+  try {
+    setState((state) => ({ ...state, loading: true }));
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    setState((state) => ({ ...state, data, loading: false }));
+  } catch (error) {
+    setState((state) => ({ ...state, error, loading: false }));
+  }
+};
--- src/components/Filter/Topic/index.jsx
+++ src/components/Filter/Topic/index.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import { topics, authors } from './handlers';
+import React, { useEffect, useState } from 'react';
+import { topics, authors, getData } from './handlers';
 
 const TopicFilter = () => {
+  const [state, setState] = useState({
+    data: null,
+    loading: false,
+    error: null,
+  });
   const [selectedFilter, setSelectedFilter] = useState({
     topic: 'TODOS',
     author: 'TODAS',
@@ -21,66 +26,108 @@ const TopicFilter = () => {
     });
   };
 
+  useEffect(() => {
+    getData(state, setState);
+  }, []);
+
   return (
     <nav className='navbar navbar-expand-lg bg-body-tertiary'>
       <div className='container-fluid'>
-        <form className='d-flex gap-2'>
-          <input aria-label='Search' className='form-control ms-2' placeholder='Título' type='search' />
+        {/* <button
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          className='navbar-toggler'
+          data-bs-target='#navbarSupportedContent'
+          data-bs-toggle='collapse'
+          type='button'
+        >
+          <span className='navbar-toggler-icon' />
+        </button> */}
 
-          <button className='btn btn-warning'>
-            <i className='bi bi-search' />
+        <div className='collapse navbar-collapse d-flex gap-2 justify-content-center' id='navbarSupportedContent'>
+          <form className='d-flex gap-2'>
+            <input aria-label='Search' className='form-control ms-2' placeholder='Título' type='search' />
+
+            <button className='btn btn-warning'>
+              <i className='bi bi-search' />
+            </button>
+          </form>
+
+          <div className='btn-group'>
+            <button
+              aria-expanded='false'
+              className='btn btn-warning dropdown-toggle'
+              data-bs-toggle='dropdown'
+              type='button'
+            >
+              Tema: <strong>{selectedFilter.topic}</strong>
+            </button>
+            <ul className='dropdown-menu'>
+              {topics.map((topic, i) => (
+                <li key={i}>
+                  <span className='dropdown-item' onClick={() => handleFilterSelection(topic, 'topic')}>
+                    {
+                      <>
+                        {selectedFilter.topic === topic.value && (
+                          <i className='bi bi-check-circle-fill me-2 text-warning' />
+                        )}
+                        {selectedFilter.topic !== topic.value && (
+                          <i className='bi bi-circle-fill me-2 text-secondary' />
+                        )}
+                      </>
+                    }
+                    {topic.name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='btn-group'>
+            <button
+              aria-expanded='false'
+              className='btn btn-warning dropdown-toggle'
+              data-bs-toggle='dropdown'
+              type='button'
+            >
+              Autor: <strong>{selectedFilter.author}</strong>
+            </button>
+            <ul className='dropdown-menu'>
+              {authors.map((author, i) => (
+                <li key={i}>
+                  <span className='dropdown-item' onClick={() => handleFilterSelection(author, 'author')}>
+                    {
+                      <>
+                        {selectedFilter.author === author.value && (
+                          <i className='bi bi-check-circle-fill me-2 text-warning' />
+                        )}
+                        {selectedFilter.author !== author.value && (
+                          <i className='bi bi-circle-fill me-2 text-secondary' />
+                        )}
+                      </>
+                    }
+                    {author.name}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <button
+            className='btn btn-warning d-flex align-items-center gap-2 text-uppercase'
+            type='button'
+            onClick={handleResetFilters}
+          >
+            <span className='d-none d-lg-block'>REINICIAR FILTROS</span>
+            <i className='bi bi-arrow-clockwise' />
           </button>
-        </form>
 
-        <div aria-label='Basic example' className='btn-group' role='group'>
-          {topics.map((topic, i) => (
-            <React.Fragment key={i}>
-              <input
-                autoComplete='off'
-                checked={selectedFilter.topic === topic.value}
-                className='btn-check'
-                id={topic.value}
-                name='btnradio'
-                type='radio'
-                value={topic.value}
-                onChange={() => handleFilterSelection(topic, 'topic')}
-              />
-              <label className='btn btn-outline-warning' htmlFor={topic.value}>
-                {topic.name}
-              </label>
-            </React.Fragment>
-          ))}
+          <button className='btn btn-warning align-items-center d-flex gap-2 text-uppercase' type='button'>
+            <span>CREAR</span>
+            <i className='bi bi-plus-square-fill' />
+          </button>
         </div>
-
-        <div aria-label='Basic example' className='btn-group' role='group'>
-          {authors.map((author, j) => (
-            <React.Fragment key={j}>
-              <input
-                autoComplete='off'
-                checked={selectedFilter.author === author.value}
-                className='btn-check'
-                id={author.value}
-                name='options'
-                type='radio'
-                value={author.value}
-                onChange={() => handleFilterSelection(author, 'author')}
-              />
-              <label className='btn btn-outline-warning' htmlFor={author.value}>
-                {author.name}
-              </label>
-            </React.Fragment>
-          ))}
-        </div>
-
-        <button className='btn btn-warning d-flex gap-2 text-uppercase' type='button' onClick={handleResetFilters}>
-          REINICIAR FILTROS
-          <i className='bi bi-arrow-clockwise' />
-        </button>
-
-        <button className='btn btn-warning d-flex gap-2 text-uppercase' type='button'>
-          CREAR
-          <i className='bi bi-plus-square-fill' />
-        </button>
       </div>
     </nav>
   );
