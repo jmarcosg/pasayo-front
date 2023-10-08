177ddbb8a786dec6b2158c382e848b3b5edf6f34
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-08 18:42:53 -0300

    ⚰️ remove react import

--- src/components/Filter/Topic/index.jsx
+++ src/components/Filter/Topic/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { topics, authors, getData } from './handlers';
 
 const TopicFilter = () => {
