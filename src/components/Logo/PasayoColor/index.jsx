6e4ea0d997f4de5d8b4d8e6134b0771055ec3900
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-11 20:40:43 -0300

    ✨ add colored pasayo logo

---
+++ src/components/Logo/PasayoColor/index.jsx
@@ -0,0 +1,11 @@
+import pasayoColorLogo from '../../../assets/img/pasayo_color.png';
+
+const LogoPasayoColor = () => {
+  return (
+    <>
+      <img alt='Logo PASAYO' className='me-2' src={pasayoColorLogo} width='7%' />
+    </>
+  );
+};
+
+export default LogoPasayoColor;
