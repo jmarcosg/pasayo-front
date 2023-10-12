b498a4787cca393eb2ea2caf3c88fa09f7c31d3c
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-11 22:52:30 -0300

    ✨ add pasayo logo

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,4 +1,5 @@
 import { Modal as M } from 'react-bootstrap';
+import LogoPasayoColor from '../Logo/PasayoColor';
 
 const Modal = ({ size, show, setShow, title, footer, children, loading }) => {
   const hide = () => setShow(false);
@@ -6,6 +7,7 @@ const Modal = ({ size, show, setShow, title, footer, children, loading }) => {
   return (
     <M centered aria-labelledby='contained-modal-title-vcenter' show={show} size={size ? size : 'lg'} onHide={hide}>
       <M.Header closeButton>
+        <LogoPasayoColor />
         <M.Title id='contained-modal-title-vcenter'>{!loading && title()}</M.Title>
       </M.Header>
       <M.Body>{children}</M.Body>
