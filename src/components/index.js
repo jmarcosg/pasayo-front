2130843d1f4cdc590c80de5d5a619968b0db6eaf
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-11 22:52:44 -0300

    ✨ export pasayo logo

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
-export { default as TopicFilter } from './Filter/Topic';
+
+export { default as LogoPasayoColor } from './Logo/PasayoColor';
