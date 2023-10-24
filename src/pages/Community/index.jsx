de78cb7d562ee52fee9c2ebacc8974f75ccd4204
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-23 22:46:38 -0300

    ✨ populate community page

--- src/pages/Community/index.jsx
+++ src/pages/Community/index.jsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import { CommunityTopicFilter } from '../../sections';
+import { ExperienceCard } from '../../components';
+import { getExperiencias } from './handlers';
 
 const Community = () => {
+  const [experiences, setExperiences] = useState({
+    data: null,
+    loading: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    getExperiencias(experiences, setExperiences);
+  }, []);
+
   return (
     <>
       <CommunityTopicFilter />
 
-      <div className='container'>
-        <h1>Community</h1>
+      <div className='container mt-4'>
+        {experiences.loading && (
+          <div className='d-flex justify-content-center'>
+            <div className='spinner-grow text-warning' role='status'>
+              <span className='visually-hidden'>Loading...</span>
+            </div>
+          </div>
+        )}
+
+        {!experiences?.loading && experiences?.data?.length > 0 && (
+          <ResponsiveMasonry columnsCountBreakPoints={{ 300: 1, 576: 1, 768: 2, 992: 3, 1200: 3, 1400: 3 }}>
+            <Masonry columnsCount={3} gutter={'2rem'}>
+              {experiences.data.map((experience, i) => (
+                <ExperienceCard key={i} data={experience} />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
+        )}
+
+        {!experiences?.loading && experiences?.data?.length === 0 && (
+          <div className='alert alert-warning' role='alert'>
+            No hay experiencias para mostrar en este momento.
+          </div>
+        )}
       </div>
     </>
   );
