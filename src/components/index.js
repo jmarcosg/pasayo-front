c3b918970fef5f847133885acf12dd66ccaf5f2e
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-23 22:45:44 -0300

    ✨ export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { Input } from './Input';
+export { default as ExperienceCard } from './Card/Experience';
+export { default as Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
