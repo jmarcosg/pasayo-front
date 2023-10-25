baac6a035da26d9c6a4391c5055fb5878bfae85c
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-25 11:47:19 -0300

    ✨ user depending button group

--- src/components/Card/Experience/index.jsx
+++ src/components/Card/Experience/index.jsx
@@ -1,4 +1,8 @@
+import { getSession } from '../../../utils/auth';
+
 const ExperiencieCard = ({ data }) => {
+  const { username } = getSession();
+
   return (
     <div className='card' style={{ minWidth: '18rem', maxWidth: '100%' }}>
       <div className='card-body text-bg-light rounded-top'>
@@ -13,27 +17,49 @@ const ExperiencieCard = ({ data }) => {
           <span className='fw-bold'>Tema:</span> {data?.tema}
         </li>
       </ul>
-      <div className='d-grid gap-3'>
-        <div aria-label='Acciones Experiencia' className='btn-group' role='group'>
-          <button className='btn btn-warning rounded-0' type='button'>
-            <i className='bi bi-puzzle' />
-          </button>
-          <button className='btn btn-warning rounded-0' type='button'>
-            <i className='bi bi-code-square' />
-          </button>
-          <button className='btn btn-warning rounded-0' type='button'>
-            <i className='bi bi-eye' />
-          </button>
+      {data?.user === username && (
+        <div className='d-grid gap-3'>
+          <div aria-label='Acciones Experiencia' className='btn-group' role='group'>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-puzzle' />
+            </button>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-code-square' />
+            </button>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-eye' />
+            </button>
+            <button className='btn btn-danger rounded-0' type='button'>
+              <i className='bi bi-trash' />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className='card-body text-bg-light d-flex flex-row-reverse gap-2'>
-        <span className='badge text-bg-danger d-flex gap-1'>
-          <i className='bi bi-person-fill' />
-          {data?.user}
-        </span>
+      )}
+
+      {data?.user !== username && (
+        <div className='d-grid gap-3'>
+          <div aria-label='Acciones Experiencia' className='btn-group' role='group'>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-puzzle' />
+            </button>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-code-square' />
+            </button>
+            <button className='btn btn-warning rounded-0' type='button'>
+              <i className='bi bi-eye' />
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className='card-body text-bg-light d-flex justify-content-end gap-2'>
         <span className='badge text-bg-secondary d-flex gap-1'>
           <i className='bi bi-signpost-fill' />
           {data?.trayecto[0]?.tema}
+        </span>
+        <span className='badge text-bg-danger d-flex gap-1'>
+          <i className='bi bi-person-fill' />
+          {data?.user}
         </span>
       </div>
     </div>
