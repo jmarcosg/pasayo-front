061431bd3ea95beb536a6c00b2d69a4726baddc9
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-10-31 20:47:05 -0300

    🚧 create trayecto

--- src/pages/Home/TopicFilter/ModalCrearExperiencia/FormCrearTrayecto/handlers.js
+++ src/pages/Home/TopicFilter/ModalCrearExperiencia/FormCrearTrayecto/handlers.js
@@ -1,6 +1,22 @@
+import { axios } from '../../../../../utils/axios';
+
 export const temasTrayectos = [
   { name: 'SECUENCIAS', value: 'SECUENCIAS' },
   { name: 'MODULARIDAD', value: 'MODULARIDAD' },
   { name: 'ALTERNATIVAS', value: 'ALTERNATIVAS' },
   { name: 'REPETITIVAS', value: 'REPETITIVAS' },
 ];
+
+export const createTrayecto = async (trayecto, setTrayecto) => {
+  try {
+    setTrayecto({ ...trayecto, loading: true });
+
+    // const { data } = await axios.post(`/trayecto`, trayecto.body);
+
+    setTrayecto({ ...trayecto, loading: false });
+    // setTrayecto({ ...trayecto, data, loading: false });
+  } catch (error) {
+    // setTrayecto({ ...trayecto, error, loading: false });
+    setTrayecto({ ...trayecto, error, loading: false });
+  }
+};
--- src/pages/Home/TopicFilter/ModalCrearExperiencia/FormCrearTrayecto/index.jsx
+++ src/pages/Home/TopicFilter/ModalCrearExperiencia/FormCrearTrayecto/index.jsx
@@ -1,14 +1,29 @@
-import { useEffect } from 'react';
-import { temasTrayectos } from './handlers';
+import { useEffect, useState } from 'react';
+import { createTrayecto, temasTrayectos } from './handlers';
 
 const FormCrearTrayecto = ({ setModalTitle, toggleModal }) => {
+  const [trayecto, setTrayecto] = useState({
+    body: {
+      titulo: '',
+      narrativa: '',
+      objetivo: '',
+      tema: '',
+    },
+    loading: false,
+    error: null,
+  });
+
+  const handleBodyChange = (e) => {
+    setTrayecto({ ...trayecto, body: { ...trayecto.body, [e.target.name]: e.target.value } });
+  };
+
   useEffect(() => {
     setModalTitle('Crear Nuevo Trayecto');
   }, []);
 
   return (
     <>
-      <form className='mt-2 px-4'>
+      <form className='mt-2 px-4' onSubmit={createTrayecto(trayecto, setTrayecto)}>
         <div className='row mt-2'>
           <div className='mb-3'>
             <label className='form-label fw-bold' htmlFor='titulo'>
@@ -22,6 +37,7 @@ const FormCrearTrayecto = ({ setModalTitle, toggleModal }) => {
                 name='titulo'
                 placeholder='Ingresá un Título'
                 type='text'
+                onChange={handleBodyChange}
               />
             </div>
             <div className='form-text' id='input-titulo'>
@@ -39,9 +55,11 @@ const FormCrearTrayecto = ({ setModalTitle, toggleModal }) => {
               <textarea
                 className='form-control'
                 id='narrativa'
+                name='narrativa'
                 placeholder='Ingresá una narrativa para esta experiencia'
                 rows={3}
                 style={{ height: '100px' }}
+                onChange={handleBodyChange}
               />
               <label className='text-body-secondary' htmlFor='narrativa'>
                 Ingresá una narrativa para este trayecto
@@ -56,16 +74,18 @@ const FormCrearTrayecto = ({ setModalTitle, toggleModal }) => {
 
         <div className='row mt-2'>
           <div className='mb-3'>
-            <label className='form-label fw-bold' htmlFor='objetivo-didactico'>
+            <label className='form-label fw-bold' htmlFor='objetivo'>
               Objetivo Didáctico
             </label>
             <div className='form-floating'>
               <textarea
                 className='form-control'
-                id='objetivo-didactico'
+                id='objetivo'
+                name='objetivo'
                 placeholder='Ingresá un objetivo didáctico para este trayecto'
                 rows={3}
                 style={{ height: '100px' }}
+                onChange={handleBodyChange}
               />
               <label className='text-body-secondary' htmlFor='plantilla'>
                 Ingresá un objetivo didáctico para este trayecto
@@ -77,17 +97,31 @@ const FormCrearTrayecto = ({ setModalTitle, toggleModal }) => {
           </div>
         </div>
 
-        <div className='row mt-2 px-3'>
-          <select aria-label='Select tema trayecto' className='form-select' defaultValue={'default'}>
-            <option disabled placeholder='Indicá el tema de tu trayecto' value={'default'}>
-              Indicá el tema de tu trayecto
-            </option>
-            {temasTrayectos.map((tema) => (
-              <option key={tema.value} value={tema.value}>
-                {tema.name}
-              </option>
-            ))}
-          </select>
+        <div className='row mt-2'>
+          <div className='mb-3'>
+            <div className='form-floating'>
+              <select
+                aria-label='Select tema trayecto'
+                className='form-select'
+                defaultValue={'default'}
+                id='tema'
+                name='tema'
+                onChange={handleBodyChange}
+              >
+                <option disabled placeholder='Indicá el tema de tu trayecto' value={'default'}>
+                  Indicá el tema de tu trayecto
+                </option>
+                {temasTrayectos.map((tema) => (
+                  <option key={tema.value} value={tema.value}>
+                    {tema.name}
+                  </option>
+                ))}
+              </select>
+              <label className='form-label fw-bold text-dark' htmlFor='tema'>
+                Tema
+              </label>
+            </div>
+          </div>
         </div>
 
         <div className='row mt-5 px-2'>
