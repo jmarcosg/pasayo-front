ec0afeb0c1d08ad523a4216ce8daa5c739113980
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-02 20:40:56 -0300

    🚚 rename text editor export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 export { default as ExperienceCard } from './Card/Experience';
-export { default as IDE } from './IDE';
 export { default as Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
+export { default as TextEditor } from './TextEditor';
 export { default as Toast } from './Toast';
 
 export { default as LogoPasayoColor } from './Logo/PasayoColor';
