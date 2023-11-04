13e818a59bca989158a4f50c163deccc65d24f42
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 16:18:16 -0300

    🔧 add shared session url

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,2 +1,3 @@
 export const URL_BACK = import.meta.env.VITE_URL_BACK;
+export const URL_SHARED_SESSION = import.meta.env.VITE_URL_SHARED_SESSION;
 export const APP_NAME = import.meta.env.VITE_APP_NAME;
