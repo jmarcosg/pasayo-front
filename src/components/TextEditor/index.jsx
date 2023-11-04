a5dae64ad5a7c3e0d9bc8d5c61d9741fb1e36f17
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 15:37:11 -0300

    🚧 render list when is session

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -69,13 +69,17 @@ const TextEditor = ({ code, setCode, isSession }) => {
           )}
         </div>
       )}
-      <h5 className='mt-2'>Usuarios Conectados</h5>
-      <div className='d-grid gap-2 justify-content-start'>
-        <span className='badge bg-dark fs-5 d-flex gap-2'>
-          <i className='bi bi-person-fill' />
-          <span>user</span>
-        </span>
-      </div>
+      {isSession && (
+        <>
+          <h5 className='mt-2'>Usuarios Conectados</h5>
+          <div className='d-grid gap-2 justify-content-start'>
+            <span className='badge bg-dark fs-5 d-flex gap-2'>
+              <i className='bi bi-person-fill' />
+              <span>user</span>
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
