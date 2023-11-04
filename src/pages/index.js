78d870ac4e7300308f69adca4f214335cec8560c
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 16:18:44 -0300

    ✨ export shared session page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login';
 export { default as Home } from './Home';
 export { default as TextCoding } from './TextCoding';
+export { default as TextCodingSession } from './TextCodingSession';
