dd279e4bf7178367c8f66b26bd4364fa6fafeadf
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 13:10:53 -0300

    ✨ populate experience card

--- src/pages/TextCoding/index.jsx
+++ src/pages/TextCoding/index.jsx
@@ -33,12 +33,19 @@ const TextCoding = () => {
       <div className='container mt-2'>
         <div className='card mb-2'>
           <div className='card-header'>
-            <span className='fw-bold'>Featured</span>
+            <span className='fw-bold'>{experiencia?.data?.titulo?.toUpperCase()}</span>
           </div>
           <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>An item</li>
-            <li className='list-group-item'>A second item</li>
-            <li className='list-group-item'>A third item</li>
+            <li className='list-group-item fst-italic d-flex gap-2'>
+              <i className='bi bi-chat-fill' /> <span>{experiencia?.data?.narrativa}</span>
+            </li>
+            <li className='list-group-item fw-bold d-flex gap-2'>
+              <i className='bi bi-bullseye' />
+              <span>{experiencia?.data?.objetivo}</span>
+            </li>
+            <li className='list-group-item'>
+              <span className='badge text-bg-secondary'>{experiencia?.data?.tema}</span>
+            </li>
           </ul>
         </div>
 
