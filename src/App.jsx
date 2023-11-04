26d7243c083cb49c5dfcaba3cad211293e127145
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 16:18:29 -0300

    ✨ add shared session route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 // import { Router } from './router';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components';
-import { Login, Home, TextCoding } from './pages';
+import { Login, Home, TextCoding, TextCodingSession } from './pages';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+          <Route element={<Login />} path='/login' />
           <Route element={<Home />} path='/' />
           <Route element={<TextCoding />} path='/texto/:id' />
-          <Route element={<Login />} path='/login' />
+          <Route element={<TextCodingSession />} path='/texto/:id/sesion/:session' />
         </Route>
       </Routes>
     </BrowserRouter>
