9c8df0b876209292cf8a014fa05a1aeb47cfc8c0
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-04 09:35:04 -0300

    ✨ text IDE now working

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -1,27 +1,59 @@
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import Editor from '@monaco-editor/react';
-import ThemeDropwdown from './ThemeDropdown';
+import { options } from './handlers';
 
-const TextEditor = ({ onChange, language, code, theme }) => {
-  const [value, setValue] = useState(code || '');
+const TextEditor = ({ code, setCode }) => {
+  const [value, setValue] = useState(code.body);
 
   const handleEditorChange = (value) => {
     setValue(value);
-    // onChange('code', value);
+    setCode({ ...code, body: value, compiled: false });
+  };
+
+  const handleRunCode = () => {
+    if (!code.body) return;
+
+    try {
+      eval(code.body);
+      toast.success('Código ejecutado correctamente');
+      setCode({ ...code, compiled: true });
+    } catch (exception) {
+      if (exception instanceof ReferenceError) {
+        toast.error('No se puede ejecutar código con variables indefinidas');
+      } else if (exception instanceof SyntaxError) {
+        toast.error('No se puede ejecutar código con errores de sintaxis');
+      }
+    }
   };
 
   return (
-    <div className='text-ide'>
-      <ThemeDropwdown />
+    <div className='row justify-content-center'>
       <Editor
-        defaultValue='// comentario'
+        defaultValue={'// Parece que no hay nada aquí...'}
         height='65vh'
-        language={language || 'javascript'}
+        language={'javascript'}
+        loading={'Cargando...'}
+        options={options}
         theme={'vs-dark'}
-        value={value}
+        value={code.body}
         width={`100%`}
-        // onChange={handleEditorChange}
+        onChange={handleEditorChange}
       />
+      {code.body && (
+        <div aria-label='Botonera' className='btn-group' role='group'>
+          <button className='btn btn-warning' onClick={handleRunCode}>
+            <i className='bi bi-play-fill' />
+          </button>
+          <button
+            className={`btn ${code.compiled ? 'btn-warning' : 'btn-outline-warning'}`}
+            disabled={!code.compiled}
+            type='button'
+          >
+            Guardar como solución
+          </button>
+        </div>
+      )}
     </div>
   );
 };
