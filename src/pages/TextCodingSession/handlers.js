8098aed6c6a10cee6a948f91490b90b10a46f8b8
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-05 23:19:05 -0300

    🚧 session creation

--- src/pages/TextCodingSession/handlers.js
+++ src/pages/TextCodingSession/handlers.js
@@ -1,5 +1,9 @@
+import io from 'socket.io-client';
 import { toast } from 'react-hot-toast';
 import { axios } from '../../utils/axios';
+import { URL_SHARED_SESSION } from '../../utils/config';
+
+const socket = io(URL_SHARED_SESSION);
 
 export const getExperiencia = async (experiencia, setExperiencia, code, setCode, id) => {
   try {
@@ -19,5 +23,45 @@ export const getExperiencia = async (experiencia, setExperiencia, code, setCode,
     setExperiencia({ ...experiencia, data, loading: false });
   } catch (error) {
     setExperiencia({ ...experiencia, error, loading: false });
+  }
+};
+
+export const createSession = async (session, setSession, id) => {
+  try {
+    setSession({ ...session, loading: true });
+
+    const promise = axios.post('/session', { id_experiencia: id });
+
+    toast.promise(promise, {
+      loading: 'Creando sesión...',
+      success: '¡Sesión creada!',
+      error: 'Ocurrio al crear la sesión.',
+    });
+
+    const { data } = await promise;
+
+    setSession({ ...session, data, loading: false });
+  } catch (error) {
+    setSession({ ...session, error, loading: false });
+  }
+};
+
+export const joinSession = async (session, setSession, id) => {
+  try {
+    setSession({ ...session, loading: true });
+
+    const promise = axios.get(`/session/${id}`);
+
+    toast.promise(promise, {
+      loading: 'Uniendose a sesión...',
+      success: '¡Me uní a la sesión!',
+      error: 'Ocurrio al unirme a la sesión.',
+    });
+
+    const { data } = await promise;
+
+    setSession({ ...session, data, loading: false });
+  } catch (error) {
+    setSession({ ...session, error, loading: false });
   }
 };
--- src/pages/TextCodingSession/index.jsx
+++ src/pages/TextCodingSession/index.jsx
@@ -4,7 +4,13 @@ import { TextEditor } from '../../components';
 import { getExperiencia } from './handlers';
 
 const TextCodingSession = () => {
-  let { id } = useParams();
+  let { id, user, session: sessionID } = useParams();
+  const [session, setSession] = useState({
+    data: null,
+    loading: false,
+    error: null,
+  });
+
   const [experiencia, setExperiencia] = useState({
     data: null,
     loading: false,
@@ -19,8 +25,18 @@ const TextCodingSession = () => {
   });
 
   useEffect(() => {
-    getExperiencia(experiencia, setExperiencia, code, setCode, id);
+    console.log(`exp id: ${id}`);
+    console.log(`user: ${user}`);
+    if (sessionID) {
+      console.log(`session id ${sessionID}`);
+    }
   }, []);
+
+  useEffect(() => {
+    if (session.data) {
+      getExperiencia(experiencia, setExperiencia, code, setCode, id);
+    }
+  }, [session]);
 
   return (
     <>
@@ -30,27 +46,41 @@ const TextCodingSession = () => {
         </Link>
       </div>
 
-      <div className='container mt-2'>
-        <div className='card mb-2'>
-          <div className='card-header'>
-            <span className='fw-bold'>{experiencia?.data?.titulo?.toUpperCase()}</span>
-          </div>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item fst-italic d-flex gap-2'>
-              <i className='bi bi-chat-fill' /> <span>{experiencia?.data?.narrativa}</span>
-            </li>
-            <li className='list-group-item fw-bold d-flex gap-2'>
-              <i className='bi bi-bullseye' />
-              <span>{experiencia?.data?.objetivo}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='badge text-bg-secondary'>{experiencia?.data?.tema}</span>
-            </li>
-          </ul>
+      {session.loading && !session.data && (
+        <div className='spinner-grow text-warning' role='status'>
+          <span className='visually-hidden'>Cargando...</span>
         </div>
+      )}
 
-        <TextEditor isSession code={code} setCode={setCode} />
-      </div>
+      {experiencia.loading && !experiencia.data && (
+        <div className='spinner-grow text-warning' role='status'>
+          <span className='visually-hidden'>Cargando...</span>
+        </div>
+      )}
+
+      {!session.loading && !experiencia.loading && experiencia.data && (
+        <div className='container mt-2'>
+          <div className='card mb-2'>
+            <div className='card-header'>
+              <span className='fw-bold'>{experiencia?.data?.titulo?.toUpperCase()}</span>
+            </div>
+            <ul className='list-group list-group-flush'>
+              <li className='list-group-item fst-italic d-flex gap-2'>
+                <i className='bi bi-chat-fill' /> <span>{experiencia?.data?.narrativa}</span>
+              </li>
+              <li className='list-group-item fw-bold d-flex gap-2'>
+                <i className='bi bi-bullseye' />
+                <span>{experiencia?.data?.objetivo}</span>
+              </li>
+              <li className='list-group-item'>
+                <span className='badge text-bg-secondary'>{experiencia?.data?.tema}</span>
+              </li>
+            </ul>
+          </div>
+
+          <TextEditor isSession code={code} setCode={setCode} />
+        </div>
+      )}
     </>
   );
 };
