6a31a47beb628c42ea3771f838d0984b44d7cf3d
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-11-06 22:57:27 -0300

    🔧 fix config constants because of env changes

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -3,6 +3,6 @@ import { default as a } from 'axios';
 import { URL_BACK } from '../config';
 
 export const axios = a.create({
-  baseURL: URL_BACK,
+  baseURL: `${URL_BACK}/api`,
   withCredentials: true,
 });
