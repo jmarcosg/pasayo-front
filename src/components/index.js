50355459133a514a512ae4e78e69b87644c94b41
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-12-03 23:56:08 -0300

    🔨 export blocks editor

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Input } from './Input';
 export { default as Layout } from './Layout';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
+export { default as BlocksEditor } from './BlocksEditor';
 export { default as TextEditor } from './TextEditor';
 export { default as Toast } from './Toast';
 export { default as Tooltip } from './Tooltip';
