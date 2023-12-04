09d822ec556712a23c6c0321e433c7b89089b473
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2023-12-03 23:57:45 -0300

    ✨ add blocks editor page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Auth } from './Auth';
 export { default as Home } from './Home';
 export { default as TextCoding } from './TextCoding';
 export { default as TextCodingSession } from './TextCodingSession';
+export { default as BlocksCodingSession } from './BlocksCodingSession';
