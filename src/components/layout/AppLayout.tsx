ec3b0630a90fb5d9ba0609d622af58c45369ca05
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2024-01-25 12:52:59 -0300

    🔥 remove unnecesary fragments

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,14 +4,12 @@ import { Header } from './header';
 
 export function AppLayout() {
   return (
-    <>
-      <div className='relative flex min-h-screen flex-col bg-background'>
-        <Header />
-        <main className='container flex-1 px-4 md:px-8'>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div className='relative flex min-h-screen flex-col bg-background'>
+      <Header />
+      <main className='container flex-1 px-4 md:px-8'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
